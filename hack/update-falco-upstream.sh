f3407f7be224dec7fa8fd8408eb3e88d859af1aa
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-07-19 16:16:17 +0200

    Update bash script

--- hack/update-falco-upstream.sh
+++ hack/update-falco-upstream.sh
@@ -1,23 +1,27 @@
+# Make sure remote is set up correctly and not outdated
 git remote add -f --no-tags upstream-copy git@github.com:giantswarm/falco-charts.git
 git fetch upstream-copy master
 git checkout master
-
 git pull
-git checkout -b update-falco-2
+
+# Update falco-exporter and commit to branch update-falco
+git checkout -b update-falco
 git checkout upstream-copy/master
 git subtree split -P falco-exporter -b temp-split-branch
-git checkout update-falco-2
+git checkout update-falco
 git subtree merge --squash -P helm/falco-app/charts/falco-exporter temp-split-branch
 git branch -D temp-split-branch
 
+# Update falco and commit to branch update-falco
 git checkout upstream-copy/master
 git subtree split -P falco -b temp-split-branch
-git checkout update-falco-2
+git checkout update-falco
 git subtree merge --squash -P helm/falco-app/charts/falco temp-split-branch
 git branch -D temp-split-branch
 
+# Update falcosidekick and commit to branch update-falco
 git checkout upstream-copy/master
 git subtree split -P falcosidekick -b temp-split-branch
-git checkout update-falco-2
+git checkout update-falco
 git subtree merge --squash -P helm/falco-app/charts/falcosidekick temp-split-branch
 git branch -D temp-split-branch
